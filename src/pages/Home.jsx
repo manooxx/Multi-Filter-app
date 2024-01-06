dfa1654a21394c3e6282d6301e755ed05351f462
manoxx <mbpi.obpl@gmail.com>
2024-01-06 13:19:07 +0530

    added homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
-    return (
-        <>
-            <div className='flex flex-col gap-3'>
-                <div className='bg-orange-40'>
-                    SearchBar
-                </div>
-                <div className='flex  gap-5'>
+  return (
+    <>
+      <div className="flex flex-col gap-3">
+        <div className="bg-orange-40">SearchBar</div>
+        <div className="flex  gap-5">
+          <div className="bg-pink-200 w-96 h-screen">FilterBar</div>
+          <div className="bg-green-400 w-full h-screen">ListBar</div>
+        </div>
+      </div>
+     
+    </>
+  );
+};
 
-                    <div className='bg-pink-200 w-96 h-screen'>
-                        FilterBar
-                    </div>
-                    <div className='bg-green-400 w-full h-screen'>
-                        ListBar
-                    </div>
-
-
-                </div>
-            </div>
-
-        </>
-
-
-    )
-}
-
-export default Home
+export default Home;
