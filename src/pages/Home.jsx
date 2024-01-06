113445c8693bf4d6f2d4c9cb3f46bf8aec263026
manoxx <mbpi.obpl@gmail.com>
2024-01-06 13:10:20 +0530

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <>
+            <div className='flex flex-col gap-3'>
+                <div className='bg-orange-40'>
+                    SearchBar
+                </div>
+                <div className='flex  gap-5'>
+
+                    <div className='bg-pink-200 w-96 h-screen'>
+                        FilterBar
+                    </div>
+                    <div className='bg-green-400 w-full h-screen'>
+                        ListBar
+                    </div>
+
+
+                </div>
+            </div>
+
+        </>
+
+
+    )
+}
+
+export default Home
